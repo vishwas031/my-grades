5939369b6867239eb5464c4d0eaa0cfcad558e44
Vishwas Singh <031vishwas@gmail.com>
2022-01-18 20:02:23 +0530

    made Admin login route and middlewares

--- Server/src/routes/admin.js
+++ Server/src/routes/admin.js
@@ -1,11 +1,16 @@
 const express = require("express")
 const router = express.Router();
+const admin = require('../models/admin')
 const {upload} = require ('../controllers/admin/upload.js')
 const {AdminLogin} = require ('../controllers/admin/AdminLogin.js')
-const authMiddleware = require("../middleware/authMiddleware")
+const isAdmin = require("../middleware/isAdmin");
 
+router.use(express.json())
 
-router.post('/AdminLogin',AdminLogin);
-router.post('/upload',authMiddleware,upload);
+router.post('/AdminLogin' ,AdminLogin)
 
-module.exports = router
+router.post('/upload',isAdmin,(req,res)=>{
+    upload
+});
+
+module.exports = router ;
--- Server/src/routes/student.js
+++ Server/src/routes/student.js
@@ -1,3 +1,19 @@
 const express = require("express")
 const router = express.Router();
-// const { }
+const {register} = require ('../controllers/student/register.js')
+const {login} = require ('../controllers/student/login.js')
+const {myResult} = require ('../controllers/student/myResult.js')
+const isLoggedIn = require("../middleware/isLoggedIn")
+
+
+router.post('/register',(req,res)=>{
+    register
+} );
+router.post('/login',(req,res)=>{
+    login
+});
+router.post('/myResult',isLoggedIn,(req,res)=>{
+    myResult
+});
+
+module.exports = router ;
